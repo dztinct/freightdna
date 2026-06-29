aabf20516256680c949a1d04fcf48076c29dc62d
Dztinct <detunjinov@gmail.com>
2026-06-29 02:34:03 +0100

    new adjustments

--- resources/views/layouts/app-new.blade.php
+++ resources/views/layouts/app-new.blade.php
@@ -46,6 +46,7 @@
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
 <meta name="theme-color" content="#ffffff">
+
     </head>
     <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
 
